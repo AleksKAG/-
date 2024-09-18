aabeb309da79c0345e92b81eb3882b2fe2e591df
AleksKAG <WyrkaloW@yandex.ru>
2024-09-19 01:44:07 +0300

    Update ftracker.go

--- ftracker.go
+++ ftracker.go
@@ -42,8 +42,8 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 		dist := float64(lengthPool*countPool) / mInKm
 		speed := swimmingMeanSpeed(lengthPool, countPool, duration)
 		calories := SwimmingSpentCalories(lengthPool, countPool, duration, weight)
-		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\нСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, dist, speed, calories)
-	default:
+		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, dist, speed, calories)
+		default:
 		return "неизвестный тип тренировки"
 	}
 }
