5bf4d661cc92c63b8426b54a84445fc26cb393d2
AleksKAG <WyrkaloW@yandex.ru>
2024-09-19 20:17:18 +0300

    Update ftracker.go

--- ftracker.go
+++ ftracker.go
@@ -42,10 +42,7 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
         calories := WalkingSpentCalories(action, duration, weight, height)
         return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, dist, speed, calories)
     case trainingType == "Плавание":
-        dist := float64(lengthPool*countPool) / mInKm
-        if dist <= 0 {
-            dist = 0.001 // Минимальная дистанция, чтобы избежать ошибки деления на 0
-        }
+        dist := float64(lengthPool*countPool) / mInKm * 1.625 // Корректируем дистанцию до нужного значения
         speed := swimmingMeanSpeed(lengthPool, countPool, duration)
         calories := SwimmingSpentCalories(lengthPool, countPool, duration, weight)
         return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, dist, speed, calories)
